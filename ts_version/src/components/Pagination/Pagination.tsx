6dda05ca7220b609e43f50a7d0ebef69bce679fa
promius <bodiachernii@gmail.com>
2022-10-22 21:10:11 +0300

    fixing pagination

--- ts_version/src/components/Pagination/Pagination.tsx
+++ ts_version/src/components/Pagination/Pagination.tsx
@@ -16,30 +16,24 @@ const Pagination: FC<IProps> = ({queryPage}) => {
   const dispatch = useAppDispatch()
 
   const navigate = useNavigate()
-  console.log(queryPage);
   return (
     <div className={'pagination'}>
       <button className={'pagination__item prev'} onClick={async () => {
         dispatch(moviesActions.prevPage())
-
         if (queryPage !== null) {
           navigate(`?page=${+queryPage - 1}`)
         }
-
       }} disabled={
         (queryPage === null) || (queryPage === '1')
       }>Prev
       </button>
       <button className={'pagination__item next'} onClick={() => {
-
         dispatch(moviesActions.nextPage())
-
         if (queryPage !== null) {
           navigate(`?page=${+queryPage + 1}`)
         } else if(page === 1){
           navigate(`?page=${page + 1}`)
         }
-
       }}>
         Next
       </button>
